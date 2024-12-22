a162514bfb4d83e2678d89d31ca513419c640e1e
Gabriel FV <gabrielvasco906@gmail.com>
2024-12-21 21:22:08 -0300

    feat: put a image and shows it

---
+++ src/components/Card/index.js
@@ -0,0 +1,13 @@
+import "./Card.css";
+
+const Card = () => {
+  return (
+    <main>
+      <div className="card-profile">
+        <img alt="Foto de perfil" src="/assets/images/profile-picture.png" />
+      </div>
+    </main>
+  );
+};
+
+export default Card;
